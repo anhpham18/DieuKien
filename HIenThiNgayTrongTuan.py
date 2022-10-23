57c0d73b5df7874f83567dd810f0609b3f6b6e29
anhpham18 <110729764+anhpham18@users.noreply.github.com>
2022-10-23 17:17:42 +0700

    "Hiển thị ngày trong tuần"

---
+++ HIenThiNgayTrongTuan.py
@@ -0,0 +1,17 @@
+so = float(input("hãy chọn một số tự nhiên từ 1 đến 7:"))
+if so == 2:
+	print("Thứ hai")
+elif so == 3:
+	print("Thứ ba")
+elif so == 4:
+	print("Thứ tư")
+elif so == 5:
+	print("Thứ năm")
+elif so == 6:
+	print("Thứ sáu")
+elif so == 7:
+	print("Thứ bảy")
+elif so == 1:
+	print("Chủ Nhật")
+else:
+    print("Lỗi, số không thỏa điều kiện đưa ra")
